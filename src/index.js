a1b3757254b6532b5794cd51b721098f005bc71b
kellmcallister <128011075+kellmcallister@users.noreply.github.com>
2023-04-06 12:16:22 -0500

    JS edited variable names and added setInterval for selected cities dropdown

--- src/index.js
+++ src/index.js
@@ -38,31 +38,36 @@ function updateTime() {
     .format("h:mm:ss A");
 }
 
-function cityUpdate(event) {
+updateTime();
+setInterval(updateTime, 1000);
+
+function currentCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
-  let cityTime = moment().tz(cityTimeZone);
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let selectedCityTime = moment().tz(cityTimeZone);
+  let selectedCityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityBox = document.querySelector("#city-box");
   cityBox.innerHTML = `
         <div class="row">
           <div class="col-6">
-            <div class="city-name">${cityName}</div>
-            <div class="city-date">${cityTime.format(
+            <div class="city-name">${selectedCityName}</div>
+            <div class="city-date">${selectedCityTime.format(
               "dddd, MMMM Do, YYYY"
-            )}</div>
-          </div>
-          <div class="col-6">
-            <div class="city-time">${cityTime.format("h:mm:ss A")}</div>
+            )}
+              </div>
+              </div>
+            <div class="col-6">
+            <div class="city-time">${selectedCityTime.format("h:mm:ss A")}</div>
           </div>
         </div>
-      
-  `;
+        <a href="/">Back to KMac's fave cities</a>
+        `;
+  setInterval(() => {
+    currentCity(event);
+  }, 1000);
 }
-updateTime();
-setInterval(updateTime, 1000);
 
 let citySelect = document.querySelector("#city-dropdown");
-citySelect.addEventListener("change", cityUpdate);
+citySelect.addEventListener("change", currentCity);
